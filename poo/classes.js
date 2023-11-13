1d999f5f5a9cf222dcfd9c5a344676a40e8f1e19
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-11-13 10:43:29 -0300

    Update classes.js

--- poo/classes.js
+++ poo/classes.js
@@ -1,10 +1,23 @@
-class Professor {
-    constructor(login, nome, senha, nascimento, foto, curriculo, siape) {
+class Aluno {
+    constructor(login, nome, senha, nascimento, foto, matricula, pontos) {
         this.login = login,
             this.nome = nome,
             this.senha = senha,
             this.nascimento = nascimento,
             this.foto = foto,
+            this.matricula = matricula,
+            this.pontos = pontos
+    }
+}
+
+class Professor {
+    constructor(login, nome, senha, nascimento, foto, especializacao, curriculo, siape) {
+        this.login = login,
+            this.nome = nome,
+            this.senha = senha,
+            this.nascimento = nascimento,
+            this.foto = foto,
+            this.especializacao = especializacao,
             this.curriculo = curriculo,
             this.siape = siape
     }
@@ -23,39 +36,69 @@ class Turma {
     }
 }
 
-class Materias {
+class Materia {
     constructor(id, nome) {
         this.id = id,
-            this.nome = nome
+            this.nome = nome,
+            this.assuntos = []
     }
 }
 
-class Assuntos {
+class Comentario {
+    constructor(id, criador, conteudo) {
+        this.id = id,
+            this.criador = criador,
+            this.conteudo = conteudo,
+            this.data = new Date
+    }
+}
+class Conteudo {
+    constructor(id, materia, professor) {
+        this.id = id,
+            this.materia = materia,
+            this.professor = professor
+    }
+}
+
+class arquivoAula extends Conteudo {
+    constructor(id, materia, professor, titulo, video, duracao) {
+        super(id, materia, professor),
+            this.titulo = titulo,
+            this.video = video,
+            this.duracao = duracao
+        this.comentarios = []
+    }
+    adicionarComentario(comentario) {
+        this.comentarios.push(comentario)
+    }
+}
+
+class Apostila extends Conteudo {
+    constructor(id, materia, professor, titulo, arquivo) {
+        super(id, materia, professor),
+            this.titulo = titulo,
+            this.arquivo = arquivo,
+            this.comentarios = []
+    }
+    adicionarComentario(comentario) {
+        this.comentarios.push(comentario)
+    }
+}
+
+class Assunto {
     constructor(id, nome, materia, descricao) {
         this.id = id,
             this.nome = nome,
             this.materia = materia,
             this.descricao = descricao,
             this.atividades = []
-        this.materiais = []
+        this.conteudos = []
     }
-    adicionarMaterias(materiais) {
-        this.materiais.push(materiais)
+    adicionarMaterias(conteudo) {
+        this.conteudos.push(conteudo)
     }
 
     adicionarAtividades(atividade) {
         this.atividades.push(atividade)
     }
 }
-
-class Biologia extends Materias {
-    constructor(id, nome,) {
-        super(id, nome)
-        this.assuntos = []
-    }
-
-    adicionarAssunto(assunto) {
-        this.assuntos.push(assunto)
-    }
-}
-
